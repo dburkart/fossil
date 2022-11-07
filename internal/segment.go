293914438169bc931cad65dba603c44165a473c8
Dana Burkart <dana.burkart@gmail.com>
2022-11-07 15:04:12 -0800

    Adds a Range() function to the Segment data structure.
    Range returs a set of events that fit within the given range (exclusive
    of start / end)

--- internal/segment.go
+++ internal/segment.go
@@ -11,19 +11,19 @@ import (
 	"time"
 )
 
-const SegmentSize int64 = 10000
-const IndexSize int64 = 100
+const SegmentSize int = 10000
+const IndexSize int = 100
 
 type tsLookup struct {
 	timestamp time.Time
-	index     int64
+	index     int
 }
 
 type Segment struct {
 	HeadTime    time.Time
 	Index       [IndexSize]tsLookup
-	indexCursor int64
-	Size        int64
+	indexCursor int
+	Size        int
 	Series      [SegmentSize]Event
 }
 
@@ -46,4 +46,46 @@ func (s *Segment) AddEvent(e Event) (bool, error) {
 	s.Size += 1
 
 	return true, nil
+}
+
+func (s *Segment) Range(begin time.Time, end time.Time) []Event {
+	var startIndex, endIndex int
+	// First, find where we must start in our segment
+	if s.HeadTime.After(begin) {
+		startIndex = 0
+	} else {
+		// TODO: This should be a binary search
+		for i := 0; i < s.indexCursor; i++ {
+			if s.Index[i].timestamp.After(begin) {
+				startIndex = s.Index[i].index
+				for j := s.Index[i-1].index; j < startIndex; j++ {
+					if s.Series[j].Timestamp.After(begin) {
+						startIndex = j
+						break
+					}
+				}
+				break
+			}
+		}
+	}
+
+	if s.Series[s.Size-1].Timestamp.Before(end) {
+		endIndex = s.Size
+	} else {
+		// TODO: This should be a binary search
+		for i := s.indexCursor - 1; i >= 0; i-- {
+			if s.Index[i].timestamp.Before(end) {
+				endIndex = s.Index[i].index
+				for j := s.Index[i+1].index; j > endIndex; j-- {
+					if s.Series[j].Timestamp.Before(end) {
+						endIndex = j
+						break
+					}
+				}
+				break
+			}
+		}
+	}
+
+	return s.Series[startIndex:endIndex]
 }
--- internal/segment_test.go
+++ internal/segment_test.go
@@ -11,21 +11,29 @@ import (
 	"time"
 )
 
-func TestAddingToSegment(t *testing.T) {
+func startTime() time.Time {
+	location, _ := time.LoadLocation("America/Los_Angeles")
+	return time.Date(2022, 11, 7, 13, 0, 0, 0, location)
+}
+
+func createFullSegment() Segment {
 	segment := Segment{}
 
-	location, _ := time.LoadLocation("America/Los_Angeles")
 	event := Event{
-		Timestamp: time.Date(2022, 11, 7, 13, 0, 0, 0, location),
+		Timestamp: startTime(),
 		Data:      "{\"foo\": 12}",
 	}
 
 	for i := 0; i < int(SegmentSize); i++ {
-		if result, err := segment.AddEvent(event); !result {
-			t.Errorf("got error when adding event: %s", err.Error())
-		}
+		segment.AddEvent(event)
 		event.Timestamp = event.Timestamp.Add(60000000000)
 	}
+
+	return segment
+}
+
+func TestAddingToSegment(t *testing.T) {
+	segment := createFullSegment()
 
 	// Ensure that there are 10000 events in our segment
 	if segment.Size != SegmentSize {
@@ -35,5 +43,15 @@ func TestAddingToSegment(t *testing.T) {
 	// Ensure that there is 1,000 indices
 	if segment.Index[IndexSize-1].index == 0 {
 		t.Errorf("expected to find 1,000 indices")
+	}
+}
+
+func TestRangeFunctionality(t *testing.T) {
+	segment := createFullSegment()
+
+	events := segment.Range(startTime(), startTime().Add(time.Hour))
+
+	if len(events) != 58 {
+		t.Errorf("expected 58 events, got %d", len(events))
 	}
 }
