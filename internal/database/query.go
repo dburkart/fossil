09a880906aa0f4e49ac7212a713b1eafd851e101
Dana Burkart <dana.burkart@gmail.com>
2022-11-14 21:39:45 -0800

    Adds a basic Query object to hold information for querying the DB.

---
+++ internal/database/query.go
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2022, Dana Burkart <dana.burkart@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+package database
+
+import "time"
+
+type TimeRange struct {
+	Start time.Time
+	End   time.Time
+}
+
+// The Query object represents a single query on a database. It contains the
+// 4 main variables of a query:
+// 		* Quantifier
+//		* Topic(s)
+//		* Time Range
+//		* Data Predicate (TODO!)
+type Query struct {
+	Quantifier string
+	Topics     []string
+	Range      *TimeRange // nil means entire history (no time range)
+}
