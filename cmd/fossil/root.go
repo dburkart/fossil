5a0e1b90654aea94638011ab60224aa87318fed9
Dana Burkart <dana.burkart@gmail.com>
2022-11-25 10:36:14 -0800

    Fix license formatting.
    GoLand was yelling at me.

--- cmd/fossil/root.go
+++ cmd/fossil/root.go
@@ -1,8 +1,9 @@
 /*
- * Copyright (c) 2022, Gideon Williams gideon@gideonw.com
+ * Copyright (c) 2022, Gideon Williams <gideon@gideonw.com>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
+
 package fossil
 
 import (
