ddbb49346d2d81987a62b08171015a2972adaff3
Dana Burkart <dana.burkart@gmail.com>
2022-11-25 21:07:40 -0800

    Stub out Token struct.
    A token is a {Type, Lexeme} pair.

---
+++ pkg/lang/token.go
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2022, Dana Burkart <dana.burkart@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+package lang
+
+type TokenType int
+
+const (
+	TOK_INVALID = iota
+	TOK_EOF
+
+	TOK_IDENTIFIER
+	TOK_KEYWORD
+	TOK_NUMBER
+	TOK_STRING
+	TOK_TOPIC
+	TOK_COMMA
+
+	TOK_PAREN_L
+	TOK_PAREN_R
+
+	TOK_ARROW
+)
+
+type Token struct {
+	Type   TokenType
+	Lexeme string
+}
