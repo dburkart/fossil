99a27afb442edbe07ef29995584afb432f7740ee
Gideon Williams <gideonw@zillowgroup.com>
2022-11-27 00:32:23 -0500

    Initial server mux approach bench

--- pkg/server/server_test.go
+++ pkg/server/server_test.go
@@ -64,3 +64,75 @@ func stub(msg proto.Message) string {
 func hash(s string) int {
 	return int(s[0])
 }
+
+func BenchmarkSwitchCommandParse(b *testing.B) {
+	mux := func(msg proto.Message) string {
+		switch msg.Command {
+		case "A":
+			return "A"
+		case "B":
+			return "B"
+		case "C":
+			return "C"
+		default:
+			return ""
+		}
+	}
+
+	tests := []proto.Message{{
+		Command: "A",
+	}, {
+		Command: "B",
+	}, {
+		Command: "C",
+	},
+	}
+
+	for i := 0; i < b.N; i++ {
+		testMsg := tests[i%len(tests)]
+		ret := mux(testMsg)
+		if ret != testMsg.Command {
+			b.Errorf("Incorrect response for %s got %s", testMsg.Command, ret)
+		}
+	}
+}
+
+func BenchmarkMapCommandParse(b *testing.B) {
+	mux := MapMux{
+		mapping: make(map[string]HandleFunc),
+	}
+
+	mux.Handle("A", stub)
+	mux.Handle("B", stub)
+	mux.Handle("C", stub)
+
+	tests := []proto.Message{{
+		Command: "A",
+	}, {
+		Command: "B",
+	}, {
+		Command: "C",
+	},
+	}
+
+	for i := 0; i < b.N; i++ {
+		testMsg := tests[i%len(tests)]
+		ret := mux.Serve(testMsg)
+		if ret != testMsg.Command {
+			b.Errorf("Incorrect response for %s got %s", testMsg.Command, ret)
+		}
+	}
+
+}
+
+type MapMux struct {
+	mapping map[string]HandleFunc
+}
+
+func (m *MapMux) Handle(command string, f HandleFunc) {
+	m.mapping[command] = f
+}
+
+func (m *MapMux) Serve(msg proto.Message) string {
+	return m.mapping[msg.Command](msg)
+}
