38548c2605155301e04a242f089c4b340a8f827c
Gideon Williams <gideonw@zillowgroup.com>
2022-12-02 17:00:12 -0500

    Update NewDatabase to incorporate db name and directory (#19)

--- pkg/database/db.go
+++ pkg/database/db.go
@@ -9,12 +9,13 @@ package database
 import (
 	"bytes"
 	"encoding/gob"
-	"io/ioutil"
-	"log"
+	"fmt"
 	"os"
 	"path/filepath"
 	"sync"
 	"time"
+
+	"github.com/rs/zerolog"
 )
 
 type Database struct {
@@ -29,11 +30,12 @@ type Database struct {
 	// Private fields
 	writeLock   sync.Mutex
 	appendCount int
+	log         zerolog.Logger
 }
 
 func (d *Database) appendInternal(data Datum) {
 	if success, _ := d.Segments[d.Current].Append(data); !success {
-		log.Fatal("We should never not have enough segments, since our write-ahead log creates them")
+		d.log.Fatal().Msg("We should never not have enough segments, since our write-ahead log creates them")
 	}
 	d.appendCount += 1
 }
@@ -73,13 +75,13 @@ func (d *Database) splatToDisk() {
 	enc := gob.NewEncoder(&encoded)
 	err := enc.Encode(d)
 	if err != nil {
-		log.Fatal("encode:", err)
+		d.log.Fatal().Err(err).Msg("error gob encoding database")
 	}
 
 	backupDBPath := filepath.Join(d.Path, "database.bak")
 	file, err := os.OpenFile(backupDBPath, os.O_TRUNC|os.O_WRONLY|os.O_CREATE, 0600)
 	if err != nil {
-		log.Fatal(err)
+		d.log.Fatal().Err(err).Msg("unable to openfile")
 	}
 	defer file.Close()
 
@@ -92,12 +94,12 @@ func (d *Database) splatToDisk() {
 	// First, overwrite the database
 	err = os.Rename(backupDBPath, filepath.Join(d.Path, "database"))
 	if err != nil {
-		log.Fatal(err)
+		d.log.Fatal().Err(err).Msg("error renaming database file")
 	}
 	// Next, zero out the WriteAheadLog
 	err = os.Remove(filepath.Join(d.Path, "wal.log"))
 	if err != nil {
-		log.Fatal(err)
+		d.log.Fatal().Err(err).Msg("error removing wal.log")
 	}
 	d.appendCount = 0
 }
@@ -248,34 +250,40 @@ func (d *Database) Retrieve(q Query) []Entry {
 	return results
 }
 
-func NewDatabase(location string) *Database {
+// NewDatabase creates a new database object in memory and creates the
+// directory and files on disk for storing the data
+// location is the base directory for creating the database
+func NewDatabase(log zerolog.Logger, name string, location string) (*Database, error) {
 	var db Database
+
+	directory := filepath.Join(location, name)
+
 	// If the path does not exist, create a new directory
-	fileinfo, err := os.Stat(location)
+	fileinfo, err := os.Stat(directory)
 	if os.IsNotExist(err) {
-		err := os.Mkdir(location, 0700)
+		err := os.Mkdir(directory, 0700)
 		if err != nil {
-			log.Fatal(err)
+			return nil, err
 		}
 	} else if !fileinfo.IsDir() {
-		log.Fatal("Supplied path is not a directory")
+		return nil, fmt.Errorf("supplied path is not a directory")
 	}
 
-	if _, err := os.Stat(filepath.Join(location, "database")); err == nil {
-		contents, err := ioutil.ReadFile(filepath.Join(location, "database"))
+	if _, err := os.Stat(filepath.Join(directory, "database")); err == nil {
+		contents, err := os.ReadFile(filepath.Join(directory, "database"))
 		if err != nil {
-			log.Fatal(err)
+			return nil, err
 		}
 
 		dec := gob.NewDecoder(bytes.NewBuffer(contents))
 		err = dec.Decode(&db)
 		if err != nil {
-			log.Fatal(err)
+			return nil, err
 		}
-	} else if _, err := os.Stat(filepath.Join(location, "wal.log")); err == nil {
+	} else if _, err := os.Stat(filepath.Join(directory, "wal.log")); err == nil {
 		db = Database{
 			Version:    1,
-			Path:       location,
+			Path:       directory,
 			Segments:   []Segment{},
 			Current:    0,
 			Topics:     make(map[string]int),
@@ -286,7 +294,7 @@ func NewDatabase(location string) *Database {
 	} else {
 		db = Database{
 			Version:    1,
-			Path:       location,
+			Path:       directory,
 			Segments:   []Segment{},
 			Current:    0,
 			Topics:     make(map[string]int),
@@ -297,5 +305,5 @@ func NewDatabase(location string) *Database {
 	if db.appendCount > SegmentSize {
 		db.splatToDisk()
 	}
-	return &db
+	return &db, nil
 }
