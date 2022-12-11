1d570b41a35e4ded3315a1247a2c8cb476185f49
Dana Burkart <dana.burkart@gmail.com>
2022-12-10 23:49:50 -0800

    Fix a panic in the parser when an empty string is passed in.
    If we try to parse an empty string, we end up passing a negative value
    to strings.Repeat(), which panics.

--- pkg/query/parser.go
+++ pkg/query/parser.go
@@ -23,9 +23,14 @@ func NewSyntaxError(t Token, m string) SyntaxError {
 }
 
 func (s *SyntaxError) FormatError(input string) string {
+	repeat := s.Location[1] - s.Location[0] - 1
+	if repeat < 0 {
+		repeat = 0
+	}
+
 	errorString := "Syntax error found in query:\n"
 	errorString += input
-	errorString += fmt.Sprintf("\n%s^%s ", strings.Repeat(" ", s.Location[0]), strings.Repeat("~", s.Location[1]-s.Location[0]-1))
+	errorString += fmt.Sprintf("\n%s^%s ", strings.Repeat(" ", s.Location[0]), strings.Repeat("~", repeat))
 	errorString += fmt.Sprintf("%s\n", s.Message)
 	return errorString
 }
