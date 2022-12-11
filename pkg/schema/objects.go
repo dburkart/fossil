5929226dccd4b6f9f092d84a876b35a1a3027961
Dana Burkart <dana.burkart@gmail.com>
2022-12-10 22:58:10 -0800

    Adds a parser for schemas, and the concept of an "Object"
    Parsing a Schema will produce a struct which conforms to the "Object"
    interface, which at this time contains a "Validate" function able to
    validate that a slice of bytes conforms to the schema object.

---
+++ pkg/schema/objects.go
@@ -0,0 +1,126 @@
+/*
+ * Copyright (c) 2022, Dana Burkart <dana.burkart@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+package schema
+
+type Object interface {
+	Validate([]byte) bool
+}
+
+type (
+	Type struct {
+		Name string
+	}
+
+	Array struct {
+		Length int
+		Type   Type
+	}
+
+	ShallowMap struct {
+		Keys   []string
+		Values []Object
+	}
+)
+
+func (t Type) Size() int {
+	switch {
+	case t.Name == "boolean":
+		return 1
+	case t.Name == "int8" || t.Name == "uint8":
+		return 1
+	case t.Name == "int16" || t.Name == "uint16":
+		return 2
+	case t.Name == "int32" || t.Name == "uint32":
+		return 4
+	case t.Name == "int64" || t.Name == "uint64":
+		return 8
+	case t.Name == "float32":
+		return 4
+	case t.Name == "float64":
+		return 8
+	case t.Name == "string":
+		return 4
+	}
+	return 0
+}
+
+func (t Type) Validate(val []byte) bool {
+	switch {
+	case t.Name == "boolean":
+		if len(val) != 1 {
+			return false
+		}
+	case t.Name == "int8" || t.Name == "uint8":
+		if len(val) != 1 {
+			return false
+		}
+	case t.Name == "int16" || t.Name == "uint16":
+		if len(val) != 2 {
+			return false
+		}
+	case t.Name == "int32" || t.Name == "uint32":
+		if len(val) != 4 {
+			return false
+		}
+	case t.Name == "int64" || t.Name == "uint64":
+		if len(val) != 8 {
+			return false
+		}
+	case t.Name == "float32":
+		if len(val) != 4 {
+			return false
+		}
+	case t.Name == "float64":
+		if len(val) != 8 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (a Array) Size() int {
+	return a.Length * a.Type.Size()
+}
+
+func (a Array) Validate(val []byte) bool {
+	typeSize := a.Type.Size()
+
+	if a.Type.Name == "string" && len(val) >= 4*a.Length {
+		return true
+	}
+
+	if len(val) != a.Length*typeSize {
+		return false
+	}
+
+	return true
+}
+
+func (m ShallowMap) Validate(val []byte) bool {
+	var size int
+	hasString := false
+	for _, val := range m.Values {
+		switch val.(type) {
+		case Type:
+			if val.(Type).Name == "string" {
+				hasString = true
+			}
+			size += val.(Type).Size()
+		case Array:
+			if val.(Array).Type.Name == "string" {
+				hasString = true
+			}
+			size += val.(Array).Size()
+		}
+	}
+
+	if hasString {
+		return len(val) >= size
+	}
+	return len(val) == size
+}
---
+++ pkg/schema/parser.go
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2022, Dana Burkart <dana.burkart@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+package schema
+
+type Parser struct {
+	Scanner Scanner
+}
+
+func (p *Parser) Parse() (Object, error) {
+
+}
