09329d16b4ee795c6a9cfc3c4627761377f7f0dc
Gideon Williams <gideonw@zillowgroup.com>
2022-12-12 20:57:22 -0500

    Add metrics for commands and connections (#77)

--- pkg/server/metrics.go
+++ pkg/server/metrics.go
@@ -13,11 +13,20 @@ import (
 
 type MetricsStore interface {
 	IncClientConnection()
+	IncRequests(db, cmd string)
+	ObserveResponseNS(db, cmd string, t int64)
 }
 
 type metricsStore struct {
 	ClientConnections prometheus.Counter
+	Requests          *prometheus.CounterVec
+	ResponseNS        *prometheus.HistogramVec
 }
+
+var (
+	DatabaseLabel = "database"
+	CommandLabel  = "cmd"
+)
 
 func NewMetricsStore() MetricsStore {
 	return &metricsStore{
@@ -25,9 +34,27 @@ func NewMetricsStore() MetricsStore {
 			Name: "fossil_client_connections",
 			Help: "The total number of client connections",
 		}),
+		Requests: promauto.NewCounterVec(prometheus.CounterOpts{
+			Name: "fossil_requests",
+			Help: "Request counts for the fossil commands",
+		}, []string{DatabaseLabel, CommandLabel}),
+		ResponseNS: promauto.NewHistogramVec(prometheus.HistogramOpts{
+			Name: "fossil_response_ns",
+			Help: "Response times on commands made against a database",
+		}, []string{DatabaseLabel, CommandLabel}),
 	}
 }
 
 func (ms *metricsStore) IncClientConnection() {
 	ms.ClientConnections.Inc()
+}
+
+func (ms *metricsStore) IncRequests(db, cmd string) {
+	ms.Requests.With(prometheus.Labels{CommandLabel: cmd, DatabaseLabel: db}).Inc()
+}
+
+func (ms *metricsStore) ObserveResponseNS(db, cmd string, t int64) {
+	ms.ResponseNS.
+		With(prometheus.Labels{CommandLabel: cmd, DatabaseLabel: db}).
+		Observe(float64(t))
 }
