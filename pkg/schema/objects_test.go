846b84077ab6467a89c3bc55db9054bf4c064516
Dana Burkart <dana.burkart@gmail.com>
2022-12-17 14:13:54 -0800

    Add a couple tests.

---
+++ pkg/schema/objects_test.go
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2022, Dana Burkart <dana.burkart@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+package schema
+
+import (
+	"encoding/binary"
+	"testing"
+)
+
+func TestType_Validate(t *testing.T) {
+	tt := Type{Name: "int8"}
+
+	if !tt.Validate([]byte{byte(12)}) {
+		t.Fail()
+	}
+
+	tt = Type{Name: "int16"}
+
+	b := make([]byte, 2)
+	binary.LittleEndian.AppendUint16(b, 12)
+	if !tt.Validate(b) {
+		t.Fail()
+	}
+}
+
+func TestArray_Validate(t *testing.T) {
+	tt := Array{Type: Type{Name: "int32"}, Length: 10}
+
+	b := make([]byte, 40)
+	if !tt.Validate(b) {
+		t.Fail()
+	}
+}
