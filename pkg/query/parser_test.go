86d09c4f280b1361add78bd1616f92139462af8c
Dana Burkart <dana.burkart@gmail.com>
2023-01-04 16:38:54 -0800

    Add some test automation to make testing the parser easier.
    This will be useful for better test coverage, especially since we are
    going to greatly expanding syntax with data pipelines.

--- pkg/query/parser_test.go
+++ pkg/query/parser_test.go
@@ -7,130 +7,17 @@
 package query
 
 import (
+	"bufio"
 	"fmt"
+	"github.com/andreyvit/diff"
+	"os"
+	"path"
+	"path/filepath"
 	"reflect"
+	"strings"
 	"testing"
 	"time"
 )
-
-func TestGarbageAfterQuery(t *testing.T) {
-	p := Parser{
-		Scanner: Scanner{
-			Input: "all and then some garbage",
-		},
-	}
-
-	_, err := p.Parse()
-	if err == nil {
-		t.Fail()
-	}
-
-	p = Parser{
-		Scanner: Scanner{
-			Input: "all  \t\n",
-		},
-	}
-
-	_, err = p.Parse()
-	if err != nil {
-		t.Fail()
-	}
-}
-
-func TestEmptyQuery(t *testing.T) {
-	p := Parser{
-		Scanner: Scanner{
-			Input: "",
-		},
-	}
-
-	_, err := p.Parse()
-	if err == nil {
-		t.Fail()
-	}
-}
-
-func TestParseAllQuantifier(t *testing.T) {
-	p := Parser{
-		Scanner: Scanner{
-			Input: "all",
-		},
-	}
-
-	ast, _ := p.Parse()
-
-	if fmt.Sprint(reflect.TypeOf(ast)) != "*query.QueryNode" {
-		t.Errorf("wanted root node to be *query.QueryNode, found %s", reflect.TypeOf(ast))
-	}
-
-	child := ast.Children()[0]
-
-	if fmt.Sprint(reflect.TypeOf(child)) != "*query.QuantifierNode" {
-		t.Errorf("wanted first child to be *query.QuantifierNode, found %s", reflect.TypeOf(child))
-	}
-
-	filters := ast.Walk(nil)
-	if len(filters) != 1 {
-		t.Errorf("wanted 1 filter, got %d instead", len(filters))
-	}
-}
-
-func TestParseTopicSelector(t *testing.T) {
-	p := Parser{
-		Scanner: Scanner{
-			Input: "in /foo/bar/baz",
-		},
-	}
-
-	ast := p.topicSelector()
-	if fmt.Sprint(reflect.TypeOf(ast)) != "*query.TopicSelectorNode" {
-		t.Errorf("wanted root node to be *query.TopicSelectorNode, found %s", reflect.TypeOf(ast))
-	}
-
-	child := ast.Children()[0]
-	if fmt.Sprint(reflect.TypeOf(child)) != "*query.TopicNode" {
-		t.Errorf("wanted first child to be *query.TopicNode, found %s", reflect.TypeOf(child))
-	}
-
-	p = Parser{
-		Scanner: Scanner{
-			Input: "in /c02f3a2a-2791-443b-a2e9-c5e29740b803/",
-		},
-	}
-
-	ast = p.topicSelector()
-	if fmt.Sprint(reflect.TypeOf(ast)) != "*query.TopicSelectorNode" {
-		t.Errorf("wanted root node to be *query.TopicSelectorNode, found %s", reflect.TypeOf(ast))
-	}
-
-	child = ast.Children()[0]
-	if fmt.Sprint(reflect.TypeOf(child)) != "*query.TopicNode" {
-		t.Errorf("wanted first child to be *query.TopicNode, found %s", reflect.TypeOf(child))
-	}
-}
-
-func TestParseTimePredicate(t *testing.T) {
-	p := Parser{
-		Scanner: Scanner{
-			Input: "since ~now",
-		},
-	}
-
-	ast := p.timePredicate()
-	if fmt.Sprint(reflect.TypeOf(ast)) != "*query.TimePredicateNode" {
-		t.Errorf("wanted root node to be *query.TimePredicateNode, found %s", reflect.TypeOf(ast))
-	}
-
-	child := ast.Children()[0]
-	if fmt.Sprint(reflect.TypeOf(child)) != "*query.TimeExpressionNode" {
-		t.Errorf("wanted first child to be *query.TimeExpressionNode, found %s", reflect.TypeOf(child))
-	}
-
-	child = child.Children()[0]
-	if fmt.Sprint(reflect.TypeOf(child)) != "*query.TimeWhenceNode" {
-		t.Errorf("wanted first child to be *query.TimeWhenceNode, found %s", reflect.TypeOf(child))
-	}
-}
 
 func TestTimeWhence(t *testing.T) {
 	p := Parser{
@@ -150,4 +37,77 @@ func TestTimeWhence(t *testing.T) {
 	if !tm.Equal(want) {
 		t.Errorf("wanted time-whence to parse to %s, got %s", want, tm)
 	}
+}
+
+func TestParse(t *testing.T) {
+	testDirectory, err := filepath.Abs("../../test/parsing/query")
+	if err != nil {
+		panic(err)
+	}
+
+	inputDirectory := path.Join(testDirectory, "input")
+	expectationDirectory := path.Join(testDirectory, "expectations")
+
+	tests, err := filepath.Glob(fmt.Sprintf("%s/*.txt", inputDirectory))
+
+	for _, test := range tests {
+		t.Run(test, func(t *testing.T) {
+			var expected string
+			expectation := path.Join(expectationDirectory, filepath.Base(test))
+			expectedBytes, err := os.ReadFile(expectation)
+			if err == nil {
+				expected = string(expectedBytes)
+			}
+
+			file, err := os.Open(test)
+			if err != nil {
+				t.Errorf("Error opening test: %s", test)
+			}
+
+			scanner := bufio.NewScanner(file)
+
+			shouldPass := false
+			scanner.Scan()
+			if strings.ToUpper(scanner.Text()) == "PASS" {
+				shouldPass = true
+			}
+
+			actual := ""
+			for scanner.Scan() {
+				p := Parser{
+					Scanner{
+						Input: scanner.Text(),
+					},
+				}
+
+				ast, err := p.Parse()
+				if shouldPass && err != nil {
+					t.Error(err)
+					continue
+				}
+				if !shouldPass && err == nil {
+					t.Errorf("Expected query to fail: %s", scanner.Text())
+					continue
+				}
+
+				if shouldPass {
+					actual += ASTToString(ast)
+				}
+			}
+
+			if os.Getenv("SHOULD_REBASE") != "" {
+				err := os.WriteFile(expectation, []byte(actual), 0666)
+				if err != nil {
+					t.Error(err)
+				}
+				expected = actual
+			}
+
+			if a, e := strings.TrimSpace(actual), strings.TrimSpace(expected); a != e {
+				t.Errorf("Expectation not met:\n%s", diff.LineDiff(e, a))
+			}
+		})
+	}
+
+	fmt.Print(tests)
 }
