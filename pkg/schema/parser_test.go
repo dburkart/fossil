129dca8b1e3926881ef9f286267c43060fed7771
Dana Burkart <dana.burkart@gmail.com>
2023-08-18 20:29:57 -0700

    Update copyright dates for PR.

--- pkg/schema/parser_test.go
+++ pkg/schema/parser_test.go
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, Dana Burkart <dana.burkart@gmail.com>
+ * Copyright (c) 2022-2023, Dana Burkart <dana.burkart@gmail.com>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
